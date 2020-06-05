122f2058589e1b2321c20e13ab8cda0c491a5f02
Erik Demaine <edemaine@mit.edu>
2020-06-04 20:22:31 -0400

    Extra api.imply to fix error?

--- package.js
+++ package.js
@@ -21,6 +21,10 @@ Package.registerBuildPlugin({
 
 Package.onUse(function(api) {
   api.use('isobuild:compiler-plugin@1.0.0', 'server');
+  // ecmascript features we use:
+  api.imply('modules', 'server');
+  api.imply('ecmascript-runtime', 'server');
+  api.imply('babel-runtime', 'server');
   // `Meteor.startup` needed to implement <head> or <body> attributes
   // (as in `static-html` package)
   api.imply('meteor@1.2.17', 'client');
